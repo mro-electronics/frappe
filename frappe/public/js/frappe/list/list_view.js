76de523678a05d9a18fd6a67588515ca9840273e
danjeremynavarro <danjeremynavarro@gmail.com>
2023-10-05 09:58:34 -0600

    Marege frappe updated

--- frappe/public/js/frappe/list/list_view.js
+++ frappe/public/js/frappe/list/list_view.js
@@ -554,6 +554,7 @@ frappe.views.ListView = class ListView extends frappe.views.BaseList {
 			</div>
 		`);
 		this.setup_new_doc_event();
+		this.list_sidebar && this.list_sidebar.reload_stats();
 		this.toggle_paging && this.$paging_area.toggle(true);
 	}
 
